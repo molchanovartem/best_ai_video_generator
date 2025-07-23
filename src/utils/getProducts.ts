51430d428a5bef3cdd23ee6c526c01f1191f0a0a
Artem Molchanov <a.molchanov@just-ai.com>
2025-07-24 00:24:12 +0600

    fix product description

--- src/utils/getProducts.ts
+++ src/utils/getProducts.ts
@@ -1,6 +1,7 @@
 export type Product = {
   id: number;
   name: string;
+  descriptionName: string;
   description: string;
   numberOfGenerations: number;
   price: number;
@@ -11,6 +12,7 @@ const modelsMap = {
         {
             id: 1,
             name: "1 - 699 ₽",
+            descriptionName: "1 генерация",
             description: "1 генерация",
             numberOfGenerations: 1,
             price: 699
@@ -18,13 +20,15 @@ const modelsMap = {
           {
             id: 2,
             name: "5 - 3375 ₽",
+            descriptionName: "5 генераций",
             description: "5 генераций",
             numberOfGenerations: 5,
             price: 3375
           },
           {
             id: 3,
-            name: "10 - 6750 ₽",
+            name: "10 - 6750 ₽",  
+            descriptionName: "10 генераций",
             description: "10 генераций",
             numberOfGenerations: 10,
             price: 6750
@@ -34,6 +38,7 @@ const modelsMap = {
         {
             id: 4,
             name: '1 - 250 ₽',
+            descriptionName: '1 генерация',
             description: '1 генерация',
             numberOfGenerations: 1,
             price: 250,
@@ -41,6 +46,7 @@ const modelsMap = {
           {
             id: 5,
             name: '5 - 999 ₽',
+            descriptionName: '5 генераций',
             description: '5 генераций по цене 4',
             numberOfGenerations: 5,
             price: 999,
@@ -48,6 +54,7 @@ const modelsMap = {
           {
             id: 6,
             name: '10 - 1875 ₽',
+            descriptionName: '10 генераций',
             description: '10 генераций со скидкой',
             numberOfGenerations: 10,
             price: 1875,
