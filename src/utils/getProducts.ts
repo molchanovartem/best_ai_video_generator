913e5eba09f893990afe11645759180e4a009208
Artem Molchanov <a.molchanov@just-ai.com>
2025-07-24 00:14:23 +0600

    fix price

--- src/utils/getProducts.ts
+++ src/utils/getProducts.ts
@@ -10,21 +10,21 @@ const modelsMap = {
     'veo-3': [
         {
             id: 1,
-            name: "1 генерация",
+            name: "1 - 699 ₽",
             description: "1 генерация",
             numberOfGenerations: 1,
             price: 699
           },
           {
             id: 2,
-            name: "5 генераций",
+            name: "5 - 3375 ₽",
             description: "5 генераций",
             numberOfGenerations: 5,
             price: 3375
           },
           {
             id: 3,
-            name: "10 генераций",
+            name: "10 - 6750 ₽",
             description: "10 генераций",
             numberOfGenerations: 10,
             price: 6750
@@ -33,21 +33,21 @@ const modelsMap = {
     'kling-v2.1': [
         {
             id: 4,
-            name: '1 генерация',
+            name: '1 - 250 ₽',
             description: '1 генерация',
             numberOfGenerations: 1,
             price: 250,
           },
           {
             id: 5,
-            name: '5 генераций (скидка 20%)',
+            name: '5 - 999 ₽',
             description: '5 генераций по цене 4',
             numberOfGenerations: 5,
             price: 999,
           },
           {
             id: 6,
-            name: '10 генераций (скидка 25%)',
+            name: '10 - 1875 ₽',
             description: '10 генераций со скидкой',
             numberOfGenerations: 10,
             price: 1875,
